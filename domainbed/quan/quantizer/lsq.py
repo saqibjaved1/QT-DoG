7aac7a9892349c7e40bdb4cef73d83f0d6c5d22d
Saqib Javed <saqib.j01@gmail.com>
2024-10-08 18:41:22 +0200

    Add files via upload

---
+++ domainbed/quan/quantizer/lsq.py
@@ -0,0 +1,58 @@
+import torch as t
+
+# Uncomment for LSQ in jupyter
+# from domainbed.quan.quantizer import Quantizer
+from .quantizer import Quantizer
+
+def grad_scale(x, scale):
+    y = x
+    y_grad = x * scale
+    return (y - y_grad).detach() + y_grad
+
+
+def round_pass(x):
+    y = x.round()
+    y_grad = x
+    return (y - y_grad).detach() + y_grad
+
+
+class LsqQuan(Quantizer):
+    def __init__(self, bit, all_positive=False, symmetric=False, per_channel=True):
+        super().__init__(bit)
+
+        if all_positive:
+            assert not symmetric, "Positive quantization cannot be symmetric"
+            # unsigned activation is quantized to [0, 2^b-1]
+            self.thd_neg = 0
+            self.thd_pos = 2 ** bit - 1
+        else:
+            if symmetric:
+                # signed weight/activation is quantized to [-2^(b-1)+1, 2^(b-1)-1]
+                self.thd_neg = - 2 ** (bit - 1) + 1
+                self.thd_pos = 2 ** (bit - 1) - 1
+            else:
+                # signed weight/activation is quantized to [-2^(b-1), 2^(b-1)-1]
+                self.thd_neg = - 2 ** (bit - 1)
+                self.thd_pos = 2 ** (bit - 1) - 1
+
+        self.per_channel = per_channel
+        self.s = t.nn.Parameter(t.ones(1))
+
+    def init_from(self, x, *args, **kwargs):
+        if self.per_channel:
+            self.s = t.nn.Parameter(
+                x.detach().abs().mean(dim=list(range(1, x.dim())), keepdim=True) * 2 / (self.thd_pos ** 0.5))
+        else:
+            self.s = t.nn.Parameter(x.detach().abs().mean() * 2 / (self.thd_pos ** 0.5))
+
+    def forward(self, x):
+        if self.per_channel:
+            s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
+        else:
+            s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
+        s_scale = grad_scale(self.s, s_grad_scale)
+        x = x / s_scale
+        x = t.clamp(x, self.thd_neg, self.thd_pos)
+        x = round_pass(x)
+        x = x * s_scale
+        return x
---
+++ domainbed/util/__init__.py
@@ -0,0 +1,1 @@
+from .config import init_logger, get_config
